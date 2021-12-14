7adbfffa39a11a212d4f2d44672ab993fcc4e279
maryferrer <mary.ferrer6@gmail.com>
2021-12-14 12:57:07 -0800

    add React

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-const Pet = () => {
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Pet = (props) => {
     return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Xena"),
-      React.createElement("h2", {}, "Dog"),
-      React.createElement("h2", {}, "Pitbull"),
+      React.createElement("h1", {}, props.name),
+      React.createElement("h2", {}, props.animal),
+      React.createElement("h2", {}, props.breed),
     ]);
   };
   
   const App = () => {
     return React.createElement("div", {}, [
       React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet),
+      React.createElement(Pet, { name: "Xena", animal: "Dog", breed: "Pitbull"}),
+      React.createElement(Pet, { name: "Pickles", animal: "Cat", breed: "American Shorthair"}),
+      React.createElement(Pet, { name: "Rusty", animal: "Dog", breed: "Chihuahua-Yorkie Mix"}),
+    
     ]);
   };
   
