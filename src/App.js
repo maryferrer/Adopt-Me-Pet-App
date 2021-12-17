b09cfeecc8bc61dbe6f30c5218d7fe0801ef21c0
maryferrer <85918290+maryferrer@users.noreply.github.com>
2021-12-16 17:39:28 -0800

    add babel, create custom hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from "react";
 import ReactDOM from "react-dom";
 
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, props.name),
-      React.createElement("h2", {}, props.animal),
-      React.createElement("h2", {}, props.breed),
-    ]);
-  };
+import SearchParams from "./SearchParams";
+  
+  // const App = () => {
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, "Adopt Me!"),
+  //     React.createElement(Pet, { name: "Xena", animal: "Dog", breed: "Pitbull"}),
+  //     React.createElement(Pet, { name: "Pickles", animal: "Cat", breed: "American Shorthair"}),
+  //     React.createElement(Pet, { name: "Rusty", animal: "Dog", breed: "Chihuahua-Yorkie Mix"}),
+    
+  //   ]);
+  // };
   
   const App = () => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, { name: "Xena", animal: "Dog", breed: "Pitbull"}),
-      React.createElement(Pet, { name: "Pickles", animal: "Cat", breed: "American Shorthair"}),
-      React.createElement(Pet, { name: "Rusty", animal: "Dog", breed: "Chihuahua-Yorkie Mix"}),
-    
-    ]);
+    return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+    );
   };
-  
-  ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+  ReactDOM.render(<App />, document.getElementById("root"));
