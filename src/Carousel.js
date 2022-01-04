8a3b1f8b93184e9d37ef57de0b2ef680f4925b16
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-04 10:08:54 -0800

    event handler on click, pet images on details page

--- src/Carousel.js
+++ src/Carousel.js
@@ -6,8 +6,14 @@ class Carousel extends Component {
     };
 
     static defaultProps = {
-        images: ['http://pets-images.dev-apis.com/pets/none.jpg']
-    }
+        images: ['http://pets-images.dev-apis.com/pets/none.jpg'],
+    };
+
+    handleIndexClick = (event) => {
+        this.setState({
+            active: +event.target.dataset.index,
+        });
+    };
 
     render () {
         const { active } = this.state;
@@ -18,9 +24,12 @@ class Carousel extends Component {
                 <img src={images[active]} alt="animal" />
                 <div className="carousel-smaller">
                     {images.map((photo, index) => (
+                        // eslint-disable-next-line
                         <img
                             ket={photo}
                             src={photo}
+                            data-index={index}
+                            onClick={this.handleIndexClick}
                             className={index === active ? "active" : ""}
                             alt="animal thumbnail"
                             />
