fbbee672ce9466b0b29478252a6ca743108078cd
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-04 14:15:36 -0800

    added modal for pet adoption

--- src/Details.js
+++ src/Details.js
@@ -2,9 +2,10 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 
 class Details extends Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   async componentDidMount() {
     const res = await fetch(
@@ -14,9 +15,13 @@ class Details extends Component {
     this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+  adopt = () => (window.location = "http://bit.ly/pet-adopt");
+
   render() {
     if (this.state.loading) {
-      return <h2>loading… </h2>;
+      return <h2>loading … </h2>;
     }
 
     const {
@@ -27,6 +32,7 @@ class Details extends Component {
       description,
       name,
       images,
+      showModal,
     } = this.state;
 
     return (
@@ -35,8 +41,23 @@ class Details extends Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
-              <button>Adopt {name}</button>
+              <button
+                onClick={this.toggleModal}
+              >
+                Adopt {name}
+              </button>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
